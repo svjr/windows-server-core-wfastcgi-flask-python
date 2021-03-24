2869e143f7e61bf7c5b0913a3b71bc6cc0eae0da
marcelo.carmo <marcelo.carmo@arcelormittal.com.br>
2021-03-24 01:18:25 -0300

    correção de bugs no ModeloAnalitico.py

--- src/model/ModeloAnalitico.py
+++ src/model/ModeloAnalitico.py
@@ -1,24 +1,36 @@
-from datetime import time
+import time
 
 from joblib import load, dump
 from sklearn.base import BaseEstimator, ClassifierMixin
 import pandas as pd
 
-from src.common.app_common import name_folder_generated
+from src.common.app_common import path_app_model_generated
 
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVC
+from sklearn.decomposition import PCA
+
+#
+# Modelo de exemplo para classificar dígitos do dataset sklearn.datasets.load_digits()
+# Um notebook de exeplo de treino pode ser verificado na pasta de _dev, derivado do
+# site da sklearn https://scikit-learn.org/stable/auto_examples/classification/plot_digits_classification.html
+#
 
 class BaseModel(BaseEstimator, ClassifierMixin):
 
-    def __init__(self, file_model=None, file_dataframe=None, ):
-        self.__model = None  # Aqui o modelo pode ser inicializado por default
+    def __init__(self, file_model=None, file_dataframe=None ):
+        self.file_model = file_model
+        self.file_dataframe = file_dataframe
+        self.__model = make_pipeline(PCA(n_components=0.95),
+                                     SVC(gamma=0.001))
+                                     # Aqui o modelo pode ser inicializado por default,
+                                     # ou pode ser completado apos processo de fit()
         self.__df = None
-        self.__file_model = file_model
-        self.__file_dataframe = file_dataframe
-        if not (self.__file_model is None): self.load_model()
-        if not (self.__file_dataframe is None): self.load_dataframe()
+        if not (self.file_model is None): self.load_model()
+        if not (self.file_dataframe is None): self.load_dataframe()
 
     def fit(self, X, y):
-        self.__model.fit(X, y)
+        self.__model.fit(X , y)
         # Este template não está preparado para treino
         # Um pipeline precisa ser criado aqui para completar a aplicação com treino
         return self
@@ -27,26 +39,36 @@ class BaseModel(BaseEstimator, ClassifierMixin):
         if X is None: X = self.__df
         if X is None: raise Exception("Data Not Found")
         if not isinstance(X, pd.DataFrame):
-            return self.__model.predict(X)
+            df_result = pd.DataFrame(self.__model.predict(X), columns=['predict'])
         else:
             df_result = pd.DataFrame(self.__model.predict(X),index=X.index, columns=['predict'])
-            return df_result
+        
+        return df_result
 
     def pre_process(self, df):
         return df
 
-    def load_model(self):
-        self.__model = load(open(self.__file_model, 'rb'))
+    def load_model(self,file_model=None):
+        if file_model is None:
+            self.__model = load(open(self.file_model, 'rb'))
+        else:
+            self.__model = load(open(file_model, 'rb'))
 
-    def load_dataframe(self):
-        # Primeiro coluna sempre é um índice.
-        df = pd.read_csv(self.__file_dataframe, index_col=0)
-        self.__df = self.pre_process(df)
+    def load_dataframe(self,file_dataframe=None):
+        if file_dataframe is None:
+            # Primeiro coluna sempre é um índice.
+            df = pd.read_csv(self.file_dataframe, index_col=0)
+            self.__df = self.pre_process(df)
+        else:
+            # Primeiro coluna sempre é um índice.
+            df = pd.read_csv(file_dataframe, index_col=0)
+            self.__df = self.pre_process(df)
+        
         return self.__df
 
     def save_model(self):
         timestr = time.strftime("%Y%m%d_%H%M%S")
-        filename = name_folder_generated + "/app_base_model_v" + '_' + timestr + '.joblib'
+        filename = path_app_model_generated + "/app_base_model_v" + '_' + timestr + '.joblib'
         f = open(filename, 'wb')
         dump(self.__model, f, compress=3)
         f.close()
